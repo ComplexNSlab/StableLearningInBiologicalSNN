b808a4b916a07cc359c1b0aa8f8ffb30cd2b70fb
ArshiaRazavi <seyedarshia.razavi@ucalgary.ca>
2024-01-16 16:21:58 -0700

    SmwusNetwork class! --v02-- some features converted to properties.

--- Pycharm Version/SmwusNetwork.py
+++ Pycharm Version/SmwusNetwork.py
@@ -29,10 +29,40 @@ class SmwusNetwork:
         # desired rate for 'Rate control' state
         self.phi0 = 2 * np.random.rand(n, 1) - 1
 
+        self.kisi = np.random.normal(loc=0, scale=1 / self.N, size=(self.N, self.N))
+
+    @property
+    def phi(self):
+        return np.tanh(self.x)
+
+    @property
+    def phi_pre(self):
+        return np.tanh(self.x - self.x_bar)
+
+    @property
+    def phi_post(self):
+        return self.phi
+
+    @property
+    def learning_rate(self):
+        return np.matmul(self.phi, self.y.T) - np.matmul(self.y, self.phi.T)
+
+    @property
+    def fluctuation_rate(self):
+        beta = 0.1
+
+        if self.homeostasis_state == 'Dissipation':
+            return self.kisi - beta * self.W
+        elif self.homeostasis_state == 'Rate control':
+            return self.kisi + np.matmul(self.phi0 - self.phi, self.phi.T) * self.W
+        elif self.homeostasis_state == 'Decorrelation':
+            return self.kisi + np.identity(self.N) - np.matmul(self.phi_post, self.phi_pre.T)
+        else:
+            raise Exception("The state of network has been changed and is invalid!")
+
     def update_network(self, b: np.ndarray = None):
         dt = 0.1
         eta = 0.01
-        beta = 0.1
 
         # time scales for lowpass filter signals (x_bar and y)
         tau = 50
@@ -41,31 +71,8 @@ class SmwusNetwork:
         if b is None:
             b = np.zeros([self.N, 1])
 
-        def phi():
-            return np.tanh(self.x)
-
-        def phi_pre():
-            return np.tanh(self.x - self.x_bar)
-
-        def phi_post():
-            return phi()
-
-        def learning_rate():
-            return np.matmul(phi(), self.y.T) - np.matmul(self.y, phi().T)
-
-        def fluctuation_rate():
-            kisi = np.random.normal(loc=0, scale=1 / self.N, size=(self.N, self.N))
-            if self.homeostasis_state == 'Dissipation':
-                return kisi - beta * self.W
-            elif self.homeostasis_state == 'Rate control':
-                return kisi + np.matmul(self.phi0 - phi(), phi().T) * self.W
-            elif self.homeostasis_state == 'Decorrelation':
-                return kisi + np.identity(self.N) - np.matmul(phi_post(), phi_pre().T)
-            else:
-                raise Exception("The state of network has been changed and is invalid!")
-
-        dx = -self.x + np.matmul(self.W, phi()) + b
-        dw = eta * (fluctuation_rate() + learning_rate())
+        dx = -self.x + np.matmul(self.W, self.phi) + b
+        dw = eta * (self.fluctuation_rate + self.learning_rate)
         dy = (self.x - self.y) / tau
         dx_bar = (self.x - self.x_bar) / tau_x
 
@@ -73,3 +80,4 @@ class SmwusNetwork:
         self.W = self.W + dw * dt
         self.y = self.y + dy * dt
         self.x_bar = self.x_bar + dx_bar * dt
+        self.kisi = np.random.normal(loc=0, scale=1 / self.N, size=(self.N, self.N))
