96f97e82b76df13e2cb4a8c11022c472ed0608d3
ArshiaRazavi <seyedarshia.razavi@ucalgary.ca>
2024-01-16 13:57:34 -0700

    -- version 02 --
    all functions for dynamics of system are written.

--- Pycharm Version/SMWUS_Pycharm.py
+++ Pycharm Version/SMWUS_Pycharm.py
@@ -1,58 +1,64 @@
 import numpy as np
 
-eta = 0.01
 dt = 0.1
-
+eta = 0.01
 beta = 0.1
 N = 128
 state = 'Rate control'
 
-# Connectivity Matrix
+# time scales for lowpass filter signals (x_bar and y)
+tau = 50
+tau_x = 20
+
+# Connectivity Matrix of the network
 W = 2*np.random.randn(N, N)/np.sqrt(N)
 
-# State Vector
-x = np.random.randn(N,1)
+# State Vector of the network
+x = np.random.randn(N,  1)
+
+# low pass filters
 x_bar = np.copy(x)
 y = np.copy(x)
 
-phi0 =  2*np.random.rand(N, 1)- 1
+phi0 = 2*np.random.rand(N, 1) - 1
 
 
-def derivatives(b: np.ndarray) -> np.ndarray:
-    global x, W
+def update_network(b: np.ndarray):
+    global x, W, eta, dt, tau, y, tau_x, x_bar
+
+    def phi():
+        global x
+        return np.tanh(x)
+
+    def phi_pre():
+        global x, x_bar
+        return np.tanh(x - x_bar)
+
+    def phi_post():
+        return phi()
+
+    def learning_rate():
+        return np.matmul(phi(), y.T) - np.matmul(y, phi().T)
+
+    def fluctuation_rate():
+        global state, beta, N, W, x
+
+        kisi = np.random.normal(loc=0, scale=1 / N, size=(N, N))
+        if state == 'Dissipation':
+            return kisi - beta * W
+        elif state == 'Rate control':
+            return kisi + np.matmul(phi0 - phi(), phi().T) * W
+        elif state == 'Decorrelation':
+            return kisi + np.identity(N) - np.matmul(phi_post(), phi_pre().T)
+        else:
+            raise Exception("The state of network is invalid!")
+
     dx = -x + np.matmul(W, phi()) + b
-    return dx
+    dw = eta*(fluctuation_rate() + learning_rate())
+    dy = (x-y)/tau
+    dx_bar = (x-x_bar)/tau_x
 
-
-def phi():
-    global x
-    return np.tanh(x)
-
-
-def learning_rate ():
-    return np.matmul(phi(), y.T) - np.matmul(y, phi().T)
-
-
-def phi_pre():
-    global x, x_bar
-    return np.tanh(x - x_bar)
-
-
-def phi_post():
-    return phi()
-
-
-def fluctuation_rate ():
-    global state, beta, N, W, x
-
-    kisi = np.random.normal(loc=0, scale=1/N, size=(N, N))
-    if state == 'Dissipation':
-        return kisi - beta*W
-    elif state == 'Rate control':
-        return kisi + np.matmul(phi0-phi(), phi().T)*W
-    elif state == 'Decorrelation':
-        return kisi + np.identity(N) - np.matmul(phi_post(), phi_pre().T)
-    else:
-        raise Exception("The state of network is invalid!")
-
-
+    x = x + dx*dt
+    W = W + dw*dt
+    y = y + dy*dt
+    x_bar = x_bar + dx_bar*dt
