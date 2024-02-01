252000854a3e84779223d1bde07d0b5c98c2e6dc
ArshiaRazavi <seyedarshia.razavi@ucalgary.ca>
2024-02-01 16:03:18 -0700

    Izhikevich neurons are working (noise is scaled with dt + synaptic current is exponential)

--- Pycharm/Izhikevich model/Izhikevich.py
+++ Pycharm/Izhikevich model/Izhikevich.py
@@ -18,12 +18,12 @@ class IzhikevichNetwork:
 
         # initializing n, ne, ni
         self.n = n
-        ne, ni = int(0.8*n), int(0.2*n)
+        ne, ni = int(0.8 * n), int(0.2 * n)
         self.ne = ne
         self.ni = ni
 
         # initializing W
-        self.W = np.zeros((ne+ni, ne+ni))
+        self.W = np.zeros((ne + ni, ne + ni))
         self.W[:, :ne] = 0.5 * np.random.rand(ne + ni, ne)
         self.W[:, ne:ne + ni] = -1 * np.random.rand(ne + ni, ni)
 
@@ -51,13 +51,13 @@ class IzhikevichNetwork:
 
         # for loop for time integration
         dt = IzhikevichSpikingNeuron.dt
-        for i in range(int(T/2/dt)):
-            print(str(int(100*i*2*dt/T)) + ' %')
+        for i in range(int(T / dt)):
+            print(str(int(100 * i * dt / T)) + ' %')
 
             # thalamic input as a noise to each cell
-            thalamic_input = np.concatenate((5*np.random.randn(self.ne), 2*np.random.randn(self.ni)))
+            thalamic_input = np.concatenate((5 * np.random.randn(self.ne), 2 * np.random.randn(self.ni)))/np.sqrt(IzhikevichSpikingNeuron.dt)
 
-            fired = np.array([cell.fired for cell in self.cells]).T
+            fired = np.array([cell.axon_current for cell in self.cells]).T
             synaptic_input = np.matmul(self.W, fired)
 
             for cell_index, cell in enumerate(self.cells):
@@ -71,15 +71,21 @@ class IzhikevichNetwork:
         return spike_raster
 
     def get_potential_traces(self):
-        trace = np.zeros([self.ne + self.ni, int(self.cells[0].t/IzhikevichSpikingNeuron.dt)])
+        trace = np.zeros([self.ne + self.ni, int(self.cells[0].t / IzhikevichSpikingNeuron.dt)])
         for cell_index, cell in enumerate(self.cells):
-            trace[cell_index, :] = cell.trace
+            trace[cell_index, :] = cell.potential_trace
+
+        return trace
+
+    def get_axon_current_traces(self):
+        trace = np.zeros([self.ne + self.ni, int(self.cells[0].t / IzhikevichSpikingNeuron.dt)])
+        for cell_index, cell in enumerate(self.cells):
+            trace[cell_index, :] = cell.axon_current_trace
 
         return trace
 
 
 class IzhikevichSpikingNeuron:
-
     """
         Bifurcation methodologies enable us to reduce many biophysical accurate Hodgkin–Huxley-type neuronal
         models to a two-dimensional (2-D) system of ordinary differential equations of the form :
@@ -107,7 +113,9 @@ class IzhikevichSpikingNeuron:
     """
 
     threshold = 30  # mV
-    dt = 0.5  # mS
+    dt = 0.1  # mS
+    spike_current_amplitude = 0.25
+    axon_decay = 5  # mS
 
     def __init__(self, inhibitory: 'inhibitory cell' = True):
         """
@@ -128,20 +136,23 @@ class IzhikevichSpikingNeuron:
         # setting Izhikevich parameters
         if inhibitory:
             r = np.random.rand()
-            self.a, self.b = 0.02, 0.2
-            self.c, self.d = -65 + 15 * r**2, 8 - 6 * r**2
+            self._a, self._b = 0.02, 0.2
+            self._c, self._d = -65 + 15 * r ** 2, 8 - 6 * r ** 2
         else:
             r = np.random.rand()
-            self.a, self.b = 0.02 + 0.08 * r, 0.25 - 0.05 * r
-            self.c, self.d = -65, 2
+            self._a, self._b = 0.02 + 0.08 * r, 0.25 - 0.05 * r
+            self._c, self._d = -65, 2
 
         # initializing v and u
         self.v = -65
-        self.u = self.b * self.v
+        self.u = self._b * self.v
 
+        # data of the cell to be recorded
+        self._spike_timings = []
+        self._potential_trace = []
+        self._axon_trace = []
 
-        self.spike_timings = []
-        self.trace = []
+        # instantaneous time, axon current and fired status
         self.t = 0
         self.axon_current = 0
         self.fired = 0
@@ -158,26 +169,29 @@ class IzhikevichSpikingNeuron:
 
         """
 
+        # getting instantaneous derivatives
         dv, du = self._derivatives(I)
 
         # updating the neuron
-        self.v += dv*self.dt
-        dv, du = self._derivatives(I)
         self.v += dv * self.dt
+        self.u += du * self.dt
+        self.axon_current -= self.axon_current * self.dt / self.axon_decay
 
-        self.u += du*2*self.dt
+        self.t += self.dt
 
-        self.t += 2*self.dt
-        self.axon_current -= self.axon_current*self.dt/5
-
-        self.trace.append(self.v)
+        # recording important data
+        self._potential_trace.append(self.v)
+        self._axon_trace.append(self.axon_current)
 
         # checking whether the neuron spiked or not
         if self.v >= self.threshold:
-            self.spike_timings.append(self.t)
-            self.v = self.c
-            self.u = self.u + self.d
-            self.axon_current += 0.1
+            # recording spike timing history
+            self._spike_timings.append(self.t)
+
+            # updating variables after spike
+            self.v = self._c
+            self.u = self.u + self._d
+            self.axon_current += self.spike_current_amplitude
             self.fired = 1
         else:
             self.fired = 0
@@ -185,7 +199,28 @@ class IzhikevichSpikingNeuron:
     def _derivatives(self, I):
         # calculation of derivatives
         dv = (0.04 * self.v ** 2 + 5 * self.v + 140 - self.u + I)
-        du = self.a * (self.b * self.v - self.u)
+        du = self._a * (self._b * self.v - self.u)
 
         return dv, du
 
+    @property
+    def parameters(self):
+        """
+        Returns the parameters of the cell a, b, c, d
+        Returns
+        -------
+        [a, b, c, d]
+        """
+        return [self._a, self._b, self._c, self._d]
+
+    @property
+    def axon_current_trace(self):
+        return self._axon_trace
+
+    @property
+    def potential_trace(self):
+        return self._potential_trace
+
+    @property
+    def spike_timings(self):
+        return self._spike_timings
--- Pycharm/Izhikevich model/test.py
+++ Pycharm/Izhikevich model/test.py
@@ -1,10 +1,9 @@
 import matplotlib.pyplot as plt
 
-from Izhikevich import IzhikevichNetwork
+from Izhikevich import *
 
 network = IzhikevichNetwork(n=1000)
 network.simulate(T=1000)
-
 
 plt.eventplot(network.get_spike_raster(), colors='k')
 plt.xlabel('time (mS)')
@@ -13,3 +12,41 @@ plt.hlines(xmin=0, xmax=network.totalTime, y=800, colors='k', linewidth = 0.5)
 plt.title('Izhikevich model spike raster')
 plt.show()
 
+# traces = network.get_potential_traces()
+# plt.plot(traces[0, :])
+# plt.show()
+#
+#
+# traces = network.get_axon_current_traces()
+# plt.plot(traces[0, :])
+# plt.show()
+
+
+# mycell = network.cells[0]
+#
+# t, axon_current = mycell.axon_current_trace
+# plt.plot(t, axon_current)
+# plt.show()
+
+# mycell = IzhikevichSpikingNeuron(inhibitory=True)
+# dt = 0.1
+# T = 100
+# t0 = 50
+# tau = 20 # mS
+# g = 100 #
+# time_array = np.arange(0, T, dt)
+# for t in time_array:
+#     input_current = 1*(t-t0 > 0)*g*np.exp(-(t-t0)/tau)
+#     mycell.update(I=input_current)
+#
+# plt.subplot(3,1,1)
+# plt.plot(time_array, np.multiply(1*(time_array-t0 > 0), g*np.exp(-(time_array-t0)/tau)))
+# plt.ylabel('input current ')
+# plt.subplot(3,1,2)
+# plt.plot(time_array, mycell.potential_trace)
+# plt.ylabel('potential (mV)')
+# plt.subplot(3,1,3)
+# plt.plot(time_array, mycell.axon_current_trace)
+# plt.ylabel('output current')
+# plt.show()
+#
