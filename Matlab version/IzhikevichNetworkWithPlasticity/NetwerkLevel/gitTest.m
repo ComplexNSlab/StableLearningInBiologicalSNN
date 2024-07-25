da82c1267f7aa8e0cc8841f2a79c740ad16828f5
ArshiaRazavi <SeyedArshia.Razavi@ucalgary.ca>
2024-07-24 23:24:15 -0600

    Add git test file for automated git requests

--- Matlab version/IzhikevichNetworkWithPlasticity/NetwerkLevel/gitTest.m
+++ Matlab version/IzhikevichNetworkWithPlasticity/NetwerkLevel/gitTest.m
@@ -1,12 +1,12 @@
 % Define the file you want to add
 fileToAdd = string(pwd) + filesep + "gitTest.m";  % Replace with the path to your file
-
+%%
 % Add the specific file to the staging area
-system(['git add ', char(fileToAdd)]);
-
+system(['git add ', char('gitTest.m')]);
+%%
 % Optionally, commit the changes chwith a message
 commitMessage = 'Add git test file for automated git requests';
 system(['git commit -m "', commitMessage, '"']);
 %%
 branch = 'main';  % Replace with your branch name
-system(['git push -u origin ', branch]);
+system(['git push ']);
